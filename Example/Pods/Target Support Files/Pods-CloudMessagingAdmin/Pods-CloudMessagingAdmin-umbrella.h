49fdac631d223ecf92b1edae9e890d7c02b4d123
Andrew Podkovyrin <podkovyrin@gmail.com>
2019-11-07 12:16:52 +0300

    Rename admin app

--- Example/Pods/Target Support Files/Pods-CloudMessagingAdmin/Pods-CloudMessagingAdmin-umbrella.h
+++ Example/Pods/Target Support Files/Pods-CloudMessagingAdmin/Pods-CloudMessagingAdmin-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_CloudInAppMessaging_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CloudInAppMessaging_ExampleVersionString[];
+FOUNDATION_EXPORT double Pods_CloudMessagingAdminVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CloudMessagingAdminVersionString[];
 
